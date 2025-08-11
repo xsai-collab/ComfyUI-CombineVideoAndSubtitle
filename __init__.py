8ebcc4b7e1d1e6c2bce40eac86655f34406f3f7c
晓書 <351038762@qq.com>
2025-08-11 14:42:32 +0800

    Implement initial project structure and setup

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for CombineVideoAndSubtitle."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS"
+]
+
+__author__ = """lukezhang"""
+__email__ = "lukezhang187@gmail.com"
+__version__ = "0.0.1"
+
+from .nodes import NODE_CLASS_MAPPINGS
+from .nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ nodes.py
@@ -0,0 +1,11 @@
+
+
+
+NODE_CLASS_MAPPINGS = {
+    "CombineVideoAndSubtitle": CombineVideoAndSubtitle
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CombineVideoAndSubtitle": "Combine Video and Subtitle"
+}
