e6ae5ec73bd7f30a1ebf0a358ca6f836341c6e04
晓書 <351038762@qq.com>
2025-08-14 16:42:55 +0800

    更新getSubtitlesFromVideo类，增加可选参数以增强功能，完善输入参数检查，优化错误处理，添加进度条显示，确保字幕提取过程的可视化和用户体验。

--- nodes.py
+++ nodes.py
@@ -5,7 +5,11 @@ import subprocess
 import time
 import json
 import logging
+import faster_whisper
+
 from .utils.util_func import *
+from typing import List, Tuple
+from comfy.utils import ProgressBar
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -118,10 +122,44 @@ class getSubtitlesFromVideo:
             "required": {
                 "input_file_path": ("STRING", ),
                 "fast_whisper_model": ("FASTERWHISPERMODEL", ),
+                "language": ("STRING", {"default": "auto"}),
                 "output_path": ("STRING", {"default": ""}),
                 "output_filename": ("STRING", {"default": "output"}),
                 "output_format": (["srt", "ass", "ssa", "sub"], {"default": "srt"})
             },
+            "optional": {
+                "task": (["transcribe", "translate"], {"default": "transcribe"}),
+                "beam_size": ("INT", {"default": 5, "min": 1, "max": 10}),
+                "log_prob_threshold": ("FLOAT", {"default": -1.0, "min": -1.0, "max": 1.0}),
+                "no_speech_threshold": ("FLOAT", {"default": 0.6, "min": 0.0, "max": 1.0}),
+                "best_of": ("INT", {"default": 1, "min": 1, "max": 10}),
+                "patience": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 10.0}),
+                "temperature": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0}),
+                "compression_ratio_threshold": ("FLOAT", {"default": 2.0, "min": 0.0, "max": 10.0}),
+                "length_penalty": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 10.0}),
+                "repetition_penalty": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 10.0}),
+                "no_repeat_ngram_size": ("INT", {"default": 0, "min": 0, "max": 10}),
+                "prefix": ("STRING", {"default": ""}),
+                "suppress_blank": ("BOOLEAN", {"default": False}),
+                "suppress_tokens": ("STRING", {"default": ""}),
+                "max_initial_timestamp": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 10.0}),
+                "word_timestamps": ("BOOLEAN", {"default": False}),
+                "prepend_punctuations": ("STRING", {"default": "word"}),
+                "append_punctuations": ("STRING", {"default": "word"}),
+                "max_new_tokens": ("INT", {"default": -999, "min": -1000, "max": 10000}),
+                "chunk_length": ("INT", {"default": -999, "min": -1000, "max": 10000}),
+                "hallucination_silence_threshold": ("FLOAT", {"default": -999.0, "min": -1000.0, "max": 1000.0}),
+                "hotwords": ("STRING", {"default": "word"}),
+                "language_detection_threshold": ("FLOAT", {"default": -999.0, "min": -1000.0, "max": 1000.0}),
+                "language_detection_segments": ("INT", {"default": 1, "min": 1, "max": 100}),
+                "prompt_reset_on_temperature": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0}),
+                "condition_on_previous_text": ("BOOLEAN", {"default": True}),
+                "initial_prompt": ("STRING", {"default": ""}),
+                "without_timestamps": ("BOOLEAN", {"default": False}),
+                "vad_filter": ("BOOLEAN", {"default": False}),
+                "vad_parameters": ("STRING", {"default": ""}),
+                "clip_timestamp": ("STRING", {"default": ""}),
+            }
         }
     
     RETURN_TYPES = ("STRING",)
@@ -130,7 +168,8 @@ class getSubtitlesFromVideo:
     CATEGORY = "Combine Videos And Subtitles"
     OUTPUT_NODE = True
     
-    def get_subtitles_from_video(self, input_file_path, fast_whisper_model, output_path, output_filename, output_format):
+    def get_subtitles_from_video(self, input_file_path, fast_whisper_model:faster_whisper.WhisperModel, language, output_path, output_filename, output_format, **params, ) -> Tuple[List]:
+        params = self.collect_params(params)
         try:
             input_file_path = os.path.abspath(input_file_path).strip()
             if not check_path_exists(input_file_path):
@@ -138,27 +177,75 @@ class getSubtitlesFromVideo:
             if not check_path_is_file(input_file_path):
                 raise ValueError(f"Input file path is not a file: {input_file_path}")
             
-            if fast_whisper_model == "":
-                raise ValueError("Fast Whisper Model is not selected")
-            
             if output_path == "":
-                raise ValueError("Output path is not selected")
+                raise ValueError("Output path is not inputed")
             
             if output_filename == "":
-                raise ValueError("Output filename is not selected")
+                raise ValueError("Output filename is not inputed")
             
             if output_format == "":
                 raise ValueError("Output format is not selected")
             
+            if check_path_is_dir(output_path):
+                raise ValueError(f"Output path is a directory: {output_path}")
             
+            if not check_path_exists(output_path):
+                os.makedirs(output_path)
+            
+            output_file_path = os.path.join(output_path, f"{output_filename}.{output_format}")
+
+            # 使用fast_whisper_model进行语音识别
+            segments, info = fast_whisper_model.transcribe(input_file_path, language=language, **params)
+
+            # 创建进度条
+            comfy_pbar = ProgressBar(info.duration)
+
+            # 将识别结果转换为字典列表
+            transcriptions = []
+            for segment in segments:
+                comfy_pbar.update_absolute(segment.end)
+                transcriptions.append({
+                    "start": segment.start,
+                    "end": segment.end,
+                    "text": segment.text
+                })
+
+            # 将识别结果写入文件
+
+            return (output_file_path,)
+
         except Exception as e:
             raise ValueError(f"Error: {e}")
+    
+    @staticmethod
+    def collect_params(params):
+        if "suppress_tokens" in params:
+            params["suppress_tokens"] = eval(params["suppress_tokens"])
+        if "prefix" in params and not params["prefix"]:
+            params["prefix"] = None
+        if "hotwords" in params and not params["hotwords"]:
+            params["hotwords"] = None
+        if "initial_prompt" in params and not params["initial_prompt"]:
+            params["initial_prompt"] = None
+        if "vad_parameters" in params and not params["vad_parameters"]:
+            params["vad_parameters"] = None
+        if "max_new_tokens" in params and params["max_new_tokens"] == -999:
+            params["max_new_tokens"] = None
+        if "chunk_length" in params and params["chunk_length"] == -999:
+            params["chunk_length"] = None
+        if "hallucination_silence_threshold" in params and params["hallucination_silence_threshold"] == -999.0:
+            params["hallucination_silence_threshold"] = None
+        if "language_detection_threshold" in params and params["language_detection_threshold"] == -999.0:
+            params["language_detection_threshold"] = None
+        return params
 
 NODE_CLASS_MAPPINGS = {
-    "CombineVideosFromFolder": CombineVideosFromFolder
+    "CombineVideosFromFolder": CombineVideosFromFolder,
+    "getSubtitlesFromVideo": getSubtitlesFromVideo
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CombineVideosFromFolder": "Combine Videos From Folder"
+    "CombineVideosFromFolder": "Combine Videos From Folder",
+    "getSubtitlesFromVideo": "Get Subtitles From Video"
 }
